2446f41b73adc757086260bb282450431ff3a64e
kuanglab <kuan0009@umn.edu>
2019-11-01 12:39:13 -0500

    Update toy.m

--- toy.m
+++ toy.m
@@ -1,16 +1,16 @@
 clear all;clc
-opts.lambda = 10^-2;
-opts.beta = 10^-3;
-opts.thre = 0.01;
-opts.stepsize = 1;
-opts.normR = 1;
-opts.normW = 0;
-opts.rank_k = 10;
-opts.maxIter = 300;
-opts.graphRegType = 'SPG';
-nvec=[100,90,70,50];
-net_num=length(nvec);
-W=cell(net_num,1);
+opts.lambda = 10^-2; % hyperparameter of graph regularization
+opts.beta = 10^-3; % hyperparameter of L-2 regularization
+opts.thre = 0.01;  % stopping condition
+opts.stepsize = 1; % step size of multiplicative updating
+opts.normR = 1; % normalize the pairwise relations? (1: Yes, 0: No)
+opts.normW = 0; % normalize the knowledge graphs? (1: Yes, 0: No)
+opts.rank_k = 10; % CPD rank
+opts.maxIter = 300; % the maximum # of iterations
+opts.graphRegType = 'SPG'; % the type of product graph ('SPG': strong, 'TPG': tensor, 'CPG': Cartesian)
+nvec=[100,90,70,50]; % sizes of graphs
+net_num=length(nvec); % number of graphs
+W=cell(net_num,1); % store the graph adjacency matrices in a net_num x 1 cell array
 for i=1:net_num
     rng(1);
     W{i} = rand(nvec(i),nvec(i));
@@ -20,7 +20,7 @@ end
 R = cell(net_num,net_num);
 for i=1:net_num-1
     for j=i+1:net_num
-        R{i,j} = rand(nvec(i),nvec(j)); 
+        R{i,j} = rand(nvec(i),nvec(j)); % store the pairwise relations in the upper triangular of a net_num x net_num cell array
     end
 end
-Ares = GTCOPR(W,R,opts);
+Ares = GTCOPR(W,R,opts); % run GT-COPR algorithm and save the learned factor matrices in a net_num x 1 cell array
